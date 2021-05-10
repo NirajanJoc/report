0b91e071238886b26e55d96a92832748f5593ae7
NirajanJoc <Nirajanjoc19@gmail.com>
2021-05-10 17:33:31 +0545

    calculator models.py

--- calculator/admin.py
+++ calculator/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from calculator.models import Weight, Height, BmiCalculate
 # Register your models here.
+@admin.register(Weight)
+class WeightAdmin(admin.ModelAdmin):
+    list_display=('weight_range', 'weight_measure')
+
+@admin.register(Height)
+class HeightAdmin(admin.ModelAdmin):
+    list_display=('height_range', 'height_measure',)
+
+@admin.register(BmiCalculate)
+class BmiCalculateAdmin(admin.ModelAdmin):
+    list_display=('user', 'weight', 'height')
--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Weight(models.Model):
+    weight_range=models.CharField(max_length=20, unique=True)
+    weight_measure=models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return f"{self.weight_range} / {self.weight_measure}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+class Height(models.Model):
+    height_range=models.CharField(max_length=20, unique=True)
+    height_measure=models.CharField(max_length=20, unique=True)
+
+    
+    def __str__(self):
+        return f"{self.height_range} / {self.height_measure}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+    
+class BmiCalculate(models.Model):
+    user=models.CharField(max_length=50,)
+    weight=models.ForeignKey(Weight, on_delete=models.CASCADE)
+    height=models.ForeignKey(Height, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user}"
